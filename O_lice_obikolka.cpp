0fbc83215ac84d7724998cf64f3c525187a7bf47
simonatan <82508193+simonatan@users.noreply.github.com>
2021-04-25 18:45:15 +0300

    Add files via upload

---
+++ O_lice_obikolka.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const double PI=3.14;
+    double r, P, S;
+    
+    cout<<"r=";
+    cin>>r;
+    S=PI*r*r;
+    P=2*PI*r;
+    cout<<"S="<<S<<endl;
+    cout<<"P="<<P<<endl;
+    
+return 0;
+}
+
