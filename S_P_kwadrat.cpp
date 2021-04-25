1fa12241f29bcf77b965217053e4d955772d389e
simonatan <82508193+simonatan@users.noreply.github.com>
2021-04-25 18:54:47 +0300

    Add files via upload

---
+++ S_P_kwadrat.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a, P, S;
+    
+    cout<<"a=";
+    cin>>a;
+    
+    P=4*a;
+    S=a*a;
+    
+    cout<<"P="<<P<<", S="<<S<<endl;
+    
+return 0;
+}
+
